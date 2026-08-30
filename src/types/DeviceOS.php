9c071540949b0b9515adb008ffd7686120a70415
Phoenix30 <thomasgille300711@gmail.com>
2026-08-30 12:18:34 +0200

    Update DeviceOS.php, lot of things were wrong

--- src/types/DeviceOS.php
+++ src/types/DeviceOS.php
@@ -16,20 +16,16 @@ namespace pocketmine\network\mcpe\protocol\types;
 
 final class DeviceOS{
 
-	public const UNKNOWN = -1;
-	public const ANDROID = 1;
-	public const IOS = 2;
-	public const OSX = 3;
-	public const AMAZON = 4;
-	public const GEAR_VR = 5;
-	public const HOLOLENS = 6;
-	public const WINDOWS_10 = 7;
-	public const WIN32 = 8;
-	public const DEDICATED = 9;
-	public const TVOS = 10;
-	public const PLAYSTATION = 11;
-	public const NINTENDO = 12;
-	public const XBOX = 13;
-	public const WINDOWS_PHONE = 14;
-	public const LINUX = 15;
+	public const GOOGLE = 0;
+	public const IOS = 1;
+	public const OSX = 2;
+	public const AMAZON = 3;
+	public const WIN32 = 4;
+	public const DEDICATED = 5;
+	public const SONY = 6;
+	public const NINTENDO = 7;
+	public const XBOX = 8;
+	public const LINUX = 9;
+	public const UNKNOWN = 10;
+
 }

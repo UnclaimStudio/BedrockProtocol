2b9d43b5d64693037bf57606d8a67eb8ed3ad986
Phoenix30 <thomasgille300711@gmail.com>
2026-08-31 10:36:26 +0200

    Update ProtocolInfo

--- src/ProtocolInfo.php
+++ src/ProtocolInfo.php
@@ -32,11 +32,11 @@ final class ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 2169;
+	public const CURRENT_PROTOCOL = 2177;
 	/** Display version shown in the server logs. This should match the version on the game's home screen. */
-	public const MINECRAFT_VERSION = 'v26.50';
+	public const MINECRAFT_VERSION = 'v26.50.22';
 	/** Version sent on the network for client side compatibility checks. This may differ from the display version. */
-	public const MINECRAFT_VERSION_NETWORK = '1.26.50';
+	public const MINECRAFT_VERSION_NETWORK = '1.26.50.22';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;

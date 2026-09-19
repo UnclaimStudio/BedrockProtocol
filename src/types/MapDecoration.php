2f077b11b8820bd7c59dfb807c60703128b094e5
Phoenix30 <thomasgille300711@gmail.com>
2026-09-19 22:50:14 +0200

    ClientboundMapItemDataPacket fix

--- src/types/MapDecoration.php
+++ src/types/MapDecoration.php
@@ -1,20 +1,20 @@
-	<?php
+<?php
 
-	/*
-	 * This file is part of BedrockProtocol.
-	 * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/BedrockProtocol>
-	 *
-	 * BedrockProtocol is free software: you can redistribute it and/or modify
-	 * it under the terms of the GNU Lesser General Public License as published by
-	 * the Free Software Foundation, either version 3 of the License, or
-	 * (at your option) any later version.
-	 */
+/*
+ * This file is part of BedrockProtocol.
+ * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/BedrockProtocol>
+ *
+ * BedrockProtocol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 	declare(strict_types=1);
 
 	namespace pocketmine\network\mcpe\protocol\types;
 
-	use pocketmine\color\Color;
+use pocketmine\color\Color;
 
 	class MapDecoration{
 		public function __construct(
